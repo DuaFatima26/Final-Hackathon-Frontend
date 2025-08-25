04f8b761e97b917d66bc189961e664430cabbc23
DuaFatima26 <duaaghanchi10@gmail.com>
2025-08-25 15:41:31 +0500

    Portfolio Builder

--- src/components/AuthForm.jsx
+++ src/components/AuthForm.jsx
@@ -145,10 +145,7 @@ export default function AuthForm() {
               onClick={handleGoogleAuth}
               disabled={loading}
             >
-              <img
-                src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg"
-                alt="Google Logo"
-              />
+          
               Login with Google
             </button>
 
